d44534c5b0e1ceaee8b88d1dd300e9ff4fb91c36
Yule Zhang <yzhang@airtouchmedia.com>
2021-03-27 19:16:29 +0100

    debug

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 "use strict";
 module.exports = {
-   mysqlConfig: {
-        host: "localhost",     // Ordenador que ejecuta el SGBD
-        user: "root",          // Usuario que accede a la BD
-        password: "",          // Contraseña con la que se accede a la BD
-        database: "4cero4"     // Nombre de la base de datos
-   },
-   port: 3000                   // Puerto en el que escucha el servidor
+    /*mysqlConfig: {
+         host: "localhost",     // Ordenador que ejecuta el SGBD
+         user: "root",          // Usuario que accede a la BD
+         password: "",          // Contraseña con la que se accede a la BD
+         database: "4cero4"     // Nombre de la base de datos
+    },*/
+    mysqlConfig: {
+        host: "f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",     // Ordenador que ejecuta el SGBD
+        user: "tmcyr2okk49qmius",          // Usuario que accede a la BD
+        password: "tltk9wogq4wr1gqg",          // Contraseña con la que se accede a la BD
+        database: "bku1r78npvdqk3a3"     // Nombre de la base de datos
+    },
+    port: process.env.PORT || 3306                   // Puerto en el que escucha el servidor
+    //port: 3306                   // Puerto en el que escucha el servidor
 }
